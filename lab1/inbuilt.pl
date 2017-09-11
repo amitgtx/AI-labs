6f06c0efe9c7b0c1f43d590b46b9f7ce511fab76
Amit Agarwal <amitagarwal.17081996@gmail.com>
2017-09-11 16:57:41 +0530

    Adding inbuilt rules

---
+++ lab1/inbuilt.pl
@@ -0,0 +1,14 @@
+append( A, B, C)
+C is the result of appending A to B
+
+member(E,List)
+E is a member of List
+
+length(List,N)
+N is the number of elements in List
+
+permutation( A, B )
+B is a permutation of A
+
+findall( X, rule, ListOfX )
+ListOfX is a list of all X that satisfy rule
